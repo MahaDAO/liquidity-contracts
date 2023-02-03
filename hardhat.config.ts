06c7e8d811d1aea471977785aecbd70e70941dd4
Steven Enamakel <enamakel@mahadao.com>
2023-02-03 15:48:34 +0530

    updated hardhat

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -1,8 +1,49 @@
-import { HardhatUserConfig } from "hardhat/config";
-import "@nomicfoundation/hardhat-toolbox";
+import * as dotenv from "dotenv";
 
-const config: HardhatUserConfig = {
+import { HardhatUserConfig, task } from "hardhat/config";
+import "@nomicfoundation/hardhat-toolbox";
+import "@nomiclabs/hardhat-etherscan";
+import "@typechain/hardhat";
+import "hardhat-gas-reporter";
+import "solidity-coverage";
+import "hardhat-abi-exporter";
+
+// This is a sample Hardhat task. To learn how to create your own go to
+// https://hardhat.org/guides/create-task.html
+task("accounts", "Prints the list of accounts", async (_taskArgs, hre) => {
+  const accounts = await hre.ethers.getSigners();
+  for (const account of accounts) console.log(account.address);
+});
+
+dotenv.config();
+
+const config: HardhatUserConfig & any = {
   solidity: "0.8.17",
+  networks: {
+    hardhat: {
+      forking: {
+        url: `https://eth-mainnet.g.alchemy.com/v2/${process.env.ALCHEMY_API_KEY}`,
+      },
+    },
+  },
+  gasReporter: {
+    enabled: process.env.REPORT_GAS !== undefined,
+    currency: "USD",
+  },
+  etherscan: {
+    apiKey: {
+      polygon: process.env.POLYGONSCAN_API_KEY,
+      bsc: process.env.BSCSCAN_API_KEY,
+      mainnet: process.env.ETHERSCAN_API_KEY,
+      goerli: process.env.ETHERSCAN_API_KEY,
+    },
+  },
+  abiExporter: {
+    path: "./output/abis",
+    flat: true,
+    spacing: 2,
+    pretty: true,
+  },
 };
 
 export default config;
